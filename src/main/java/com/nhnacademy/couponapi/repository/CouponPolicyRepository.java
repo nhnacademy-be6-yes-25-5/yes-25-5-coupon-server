d20e9e84100d097e9166de3ad79e226b1792eb1b
임채령 <sa990422@naver.com>
2024-06-14 13:40:13 +0900

    ci test commit

--- src/main/java/com/nhnacademy/couponapi/repository/CouponPolicyRepository.java
+++ src/main/java/com/nhnacademy/couponapi/repository/CouponPolicyRepository.java
@@ -5,5 +5,5 @@
 //import org.springframework.stereotype.Repository;
 //
 //@Repository
-//public interface CouponPolicyRepository extends JpaRepository<CouponPolicy, Long> {
+//public interface CouponPolicyRepository extends JpaRepository<CouponPolicy, Long> {d
 //}
