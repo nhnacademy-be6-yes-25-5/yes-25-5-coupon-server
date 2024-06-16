2fd47ee0680b172f996a329fe4b613267109ca4e
Limchaereong <sa990422@naver.com>
2024-06-17 00:26:10 +0900

    feat : 레이어드 아키텍쳐로 변경

--- src/main/java/com/nhnacademy/couponapi/repository/CouponUsageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.couponapi.repository;
-
-import com.nhnacademy.couponapi.model.CouponUsage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CouponUsageRepository extends JpaRepository<CouponUsage, Long> {
-}
