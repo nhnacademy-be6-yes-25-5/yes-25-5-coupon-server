007b0c942589f52ad2f36112b8dc46a9e1b7dc8d
Limchaereong <sa990422@naver.com>
2024-06-17 01:04:14 +0900

    feat : 클래스 작성

--- src/main/java/com/nhnacademy/couponapi/application/service/CouponService.java
+++ src/main/java/com/nhnacademy/couponapi/application/service/CouponService.java
@@ -1,13 +1,13 @@
-package com.nhnacademy.couponapi.application.service;
-
-import com.nhnacademy.couponapi.presentation.dto.request.CreateCouponRequest;
-import com.nhnacademy.couponapi.presentation.dto.request.UseCouponRequest;
-import com.nhnacademy.couponapi.presentation.dto.response.CouponResponse;
-
-import java.util.List;
-
-public interface CouponService {
-    CouponResponse createCoupon(CreateCouponRequest request);
-    List<CouponResponse> getCouponsByUser(Long userId);
-    void useCoupon(UseCouponRequest request);
-}
+//package com.nhnacademy.couponapi.application.service;
+//
+//import com.nhnacademy.couponapi.presentation.dto.request.CreateCouponRequest;
+//import com.nhnacademy.couponapi.presentation.dto.request.UseCouponRequest;
+//import com.nhnacademy.couponapi.presentation.dto.response.CouponResponse;
+//
+//import java.util.List;
+//
+//public interface CouponService {
+//    CouponResponse createCoupon(CreateCouponRequest request);
+//    List<CouponResponse> getCouponsByUser(Long userId);
+//    void useCoupon(UseCouponRequest request);
+//}
