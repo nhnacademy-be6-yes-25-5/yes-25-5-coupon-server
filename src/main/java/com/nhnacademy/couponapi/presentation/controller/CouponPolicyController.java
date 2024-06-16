36670707c1e1e346e70066769af2019252c3471d
Limchaereong <sa990422@naver.com>
2024-06-17 01:00:29 +0900

    feat : 클래스 작성

--- src/main/java/com/nhnacademy/couponapi/presentation/controller/CouponPolicyController.java
+++ src/main/java/com/nhnacademy/couponapi/presentation/controller/CouponPolicyController.java
@@ -1,20 +1,20 @@
-package com.nhnacademy.couponapi.presentation.controller;
-
-import com.nhnacademy.couponapi.presentation.dto.request.CreateCouponPolicyRequest;
-import com.nhnacademy.couponapi.presentation.dto.response.CouponPolicyResponse;
-import com.nhnacademy.couponapi.service.CouponPolicyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/coupon-policies")
-public class CouponPolicyController {
-
-    @Autowired
-    private CouponPolicyService couponPolicyService;
-
-    @PostMapping
-    public CouponPolicyResponse createCouponPolicy(@RequestBody CreateCouponPolicyRequest request) {
-        return couponPolicyService.createCouponPolicy(request);
-    }
-}
+//package com.nhnacademy.couponapi.presentation.controller;
+//
+//import com.nhnacademy.couponapi.presentation.dto.request.CreateCouponPolicyRequest;
+//import com.nhnacademy.couponapi.presentation.dto.response.CouponPolicyResponse;
+//import com.nhnacademy.couponapi.service.CouponPolicyService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/coupon-policies")
+//public class CouponPolicyController {
+//
+//    @Autowired
+//    private CouponPolicyService couponPolicyService;
+//
+//    @PostMapping
+//    public CouponPolicyResponse createCouponPolicy(@RequestBody CreateCouponPolicyRequest request) {
+//        return couponPolicyService.createCouponPolicy(request);
+//    }
+//}
