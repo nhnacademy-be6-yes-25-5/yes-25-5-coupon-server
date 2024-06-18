d0d78ffb64a41f0ca76454668b66da48de22feaf
임채령 <sa990422@naver.com>
2024-06-18 12:26:10 +0900

    feat : Eureka 기반으로 feign client 설정

--- src/main/java/com/nhnacademy/couponapi/application/adapter/BookAdapter.java
+++ src/main/java/com/nhnacademy/couponapi/application/adapter/BookAdapter.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "book-api", url = "http://book-api")
+@FeignClient(name = "BOOK-USER-SERVER")
 public interface BookAdapter {
 
     @GetMapping("/books/{id}")
--- src/main/java/com/nhnacademy/couponapi/application/adapter/CategoryAdapter.java
+++ src/main/java/com/nhnacademy/couponapi/application/adapter/CategoryAdapter.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "category-api", url = "http://category-api")
+@FeignClient(name = "BOOK-USER-SERVER")
 public interface CategoryAdapter {
 
     @GetMapping("/categories/{id}")
