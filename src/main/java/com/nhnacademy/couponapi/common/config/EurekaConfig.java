d17b6b75046446ef4fd2218686b73b799f78b43b
임채령 <sa990422@naver.com>
2024-06-18 10:13:12 +0900

    feat: Eureka를 통한 ci/cd 구현

---
+++ src/main/java/com/nhnacademy/couponapi/common/config/EurekaConfig.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.couponapi.common.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(name = "eureka.client.enabled", havingValue = "true", matchIfMissing = true)
+@EnableDiscoveryClient
+public class EurekaConfig {
+}
