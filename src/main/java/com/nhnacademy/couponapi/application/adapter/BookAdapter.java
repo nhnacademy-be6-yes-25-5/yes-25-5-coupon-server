885eccb4f4993a3cbabe2aea32aff81443c77de3
임채령 <sa990422@naver.com>
2024-06-21 17:12:39 +0900

    feat : 인스턴스 주소 설정

--- src/main/java/com/nhnacademy/couponapi/application/adapter/BookAdapter.java
+++ src/main/java/com/nhnacademy/couponapi/application/adapter/BookAdapter.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "book-api", url = "http://book-api")
+@FeignClient(name = "bookAdapter", url = "http://133.186.153.195:8085")
 public interface BookAdapter {
 
     @GetMapping("/books/{id}")
--- src/main/java/com/nhnacademy/couponapi/application/adapter/UserAdapter.java
+++ src/main/java/com/nhnacademy/couponapi/application/adapter/UserAdapter.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "userAdaptor", url = "http://localhost:8061")
+@FeignClient(name = "userAdaptor", url = "http://133.186.153.195:8085")
 public interface UserAdapter {
 
     @GetMapping("/users/{id}")
