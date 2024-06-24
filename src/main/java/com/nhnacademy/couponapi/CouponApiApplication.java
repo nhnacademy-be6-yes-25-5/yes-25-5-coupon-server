776f16214fe91306ac39b0891646e954a0d02be3
임채령 <sa990422@naver.com>
2024-06-24 19:54:50 +0900

    feat : 도커 환경변수 설정

--- src/main/java/com/nhnacademy/couponapi/CouponApiApplication.java
+++ src/main/java/com/nhnacademy/couponapi/CouponApiApplication.java
@@ -2,10 +2,9 @@ package com.nhnacademy.couponapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableDiscoveryClient
+
 @EnableFeignClients(basePackages = "com.nhnacademy.couponapi.application.adapter")
 @SpringBootApplication
 public class CouponApiApplication {
