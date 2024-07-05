c3bb4b91697047b8e52ca90b8d6dcf3bf9e67b2b
임채령 <sa990422@naver.com>
2024-07-05 10:04:59 +0900

    feat : 도서상세페이지 쿠폰 모달 비회원 접근 가능하도록 JWT 필터 수정

---
+++ src/main/java/com/nhnacademy/couponapi/infrastructure/adapter/AuthAdaptor.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.couponapi.infrastructure.adapter;
+
+
+import com.nhnacademy.couponapi.presentation.dto.response.JwtAuthResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "authAdaptor", url = "${api.auth}/auth", configuration = FeignClient.class)
+public interface AuthAdaptor {
+
+    @GetMapping("/info")
+    JwtAuthResponse getUserInfoByUUID(@RequestParam String uuid);
+}
